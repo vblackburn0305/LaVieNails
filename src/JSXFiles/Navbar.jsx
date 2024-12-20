bfb84a9f4d40b4ba4436343a5018485d1b845bdb
vblackburn0305 <vblackburn0305@gmail.com>
2024-12-20 14:11:28 -0500

    Added a navbar.jsx file

---
+++ src/JSXFiles/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import {Link} from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
